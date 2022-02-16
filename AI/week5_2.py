ef086d59befd43c33d9ad3b1f630f4e6ae7235ca
bvsslgayathri <60476875+bvsslgayathri@users.noreply.github.com>
2022-02-16 07:44:53 +0530

    Create week5_2.py

---
+++ AI/week5_2.py
@@ -0,0 +1,30 @@
+import math
+from itertools import permutations
+
+def fun(a):
+    sum1 = 0
+    p = 0
+    for x in a[::-1]:
+        sum1 += d[x]*int(math.pow(10,p))
+        p+=1
+    return sum1
+
+a = input("Enter the string 1 : ")
+b = input("Enter the string 2 : ")
+r = input("Enter the result   : ")
+d = {}
+for x in a+b+r:
+    if x not in d.keys():
+        d[x] = 0
+print(d)
+perm = permutations([0,1,2,3,4,5,6,7,8,9],len(d))
+for i in list(perm):
+    index = 0
+    for x in d.keys():
+        print(d)
+        d[x] = i[index]
+        index+=1
+        print(d[x],'1')
+    x,y,z = fun(a),fun(b),fun(r)
+    if x+y == z:
+        print(x,y,z)
